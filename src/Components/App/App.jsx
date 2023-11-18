07b3dcb4600079499992526558fb9b8fc03ba805
VovaJun123 <121401616+VovaJun123@users.noreply.github.com>
2023-11-18 18:59:58 +0000

    add

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,16 +1,16 @@
 import { useEffect, lazy } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
-import { Layout } from './Layout';
-import { PrivateRoute } from './PrivateRoute';
-import { RestrictedRoute } from './RestrictedRoute';
+import { Layout } from '../Layout';
+import { PrivateRoute } from '../PrivateRoute';
+import { RestrictedRoute } from '../RestrictedRoute';
 import { refreshUser } from 'Redux/Auth/operations';
 import { useAuth } from 'Hooks';
 
-const HomePage = lazy(() => import('../Pages/Home'));
-const RegisterPage = lazy(() => import('../Pages/Register'));
-const LoginPage = lazy(() => import('../Pages/Login'));
-const ContactsPage = lazy(() => import('../Pages/Contacts'));
+const HomePage = lazy(() => import('../../Pages/Home'));
+const RegisterPage = lazy(() => import('../../Pages/Register'));
+const LoginPage = lazy(() => import('../../Pages/Login'));
+const ContactsPage = lazy(() => import('../../Pages/Contacts'));
 
 export const App = () => {
   const dispatch = useDispatch();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { App } from './Components/App';
+import { App } from './Components/App/App';
 import { GlobalStyle } from './GlobalStyle';
 import { store, persistor } from 'Redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
